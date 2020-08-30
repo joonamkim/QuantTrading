5fbd471478acb3653a6659e9b86b4daf68eed5bb
Joo-nam (Joo) Kim <jnk8843@gmail.com>
2020-08-30 14:10:18 +0900

    Uses threads to divide the fetching work

--- BalanceSheetFetcher.py
+++ BalanceSheetFetcher.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+from multiprocessing.pool import ThreadPool as Pool
+import time
 
 
 def get_balance_sheet(ticker):
@@ -37,20 +39,33 @@ def getPERandPBRsForAllKStocks():
     code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
     ticker_list = code_df['종목코드']
 
-    print(ticker_list)
+    print("Total ticker list size: ", len(ticker_list))
 
-    i = 0
+    start = time.time()
     dic = {}
-    for ticker in ticker_list:
-        if i == 10:
-            break
-        balance_sheet = get_balance_sheet(ticker)
-        if balance_sheet is None:
-            continue
-        value = balance_sheet
-        dic[ticker] = value
-        print(ticker, "  : ", value)
-        i += 1
+
+    pool_size = len(ticker_list)
+    pool = Pool(pool_size)
+
+    for i in range(len(ticker_list)):
+        pool.apply_async(crawl_balance_sheet_for_stock_code, (ticker_list[i], dic,))
+        print("thread " + str(i) + " started")
+
+    pool.close()
+    pool.join()
+    end = time.time()
+    print("########################")
+    print("DONE")
+    print("TOTAL TIME TAKEN = ", end - start)
+    print("########################")
+    return dic
+
+
+def crawl_balance_sheet_for_stock_code(ticker, dic):
+    balance_sheet = get_balance_sheet(ticker)
+    print(balance_sheet)
+    if ticker not in dic:
+        dic[ticker] = balance_sheet
 
 
 if __name__ == '__main__':
